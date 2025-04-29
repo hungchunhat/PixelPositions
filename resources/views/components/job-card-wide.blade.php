38988169792acdbc0679ee1b7573ec9b78266d11
hungchunhat <maxtroll2001@gmail.com>
2025-04-29 21:28:10 +0700

    first commit

---
+++ resources/views/components/job-card-wide.blade.php
@@ -0,0 +1,21 @@
+<div class="p-4 bg-white/5 rounded-xl flex flex-col text-center">
+    <div>
+        <img src="http://placehold.co/42" alt="placeholder">
+    </div>
+
+    <div>
+        <div class="self-start text-sm">
+            Laracasts
+        </div>
+        <h3>Video Producer</h3>
+        <p>Full Time - From $60,000</p>
+    </div>
+
+    <div>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+        <x-tag>Tag</x-tag>
+    </div>
+
+</div>
+
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,39 @@
+<x-layout>
+    <div class="space-y-10">
+        <x-section-heading>Featured Jobs</x-section-heading>
+        <section>
+            <div class="grid lg:grid-cols-3 gap-8">
+                <x-job-card></x-job-card>
+                <x-job-card></x-job-card>
+                <x-job-card></x-job-card>
+            </div>
+        </section>
+        <section>
+            <x-section-heading>Tags</x-section-heading>
+            <div class="mt-6 space-x-1">
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+                <x-tag>Tag</x-tag>
+            </div>
+        </section>
+        <section>
+            <x-section-heading>Recent Jobs</x-section-heading>
+            <div class="mt-6 space-y-5">
+                <x-job-card-wide></x-job-card-wide>
+                <x-job-card-wide></x-job-card-wide>
+                <x-job-card-wide></x-job-card-wide>
+                <x-job-card-wide></x-job-card-wide>
+            </div>
+        </section>
+    </div>
+</x-layout>
+
