2c18ba4ac6c0d5fa4019378164cbdfb05b215322
hungchunhat <maxtroll2001@gmail.com>
2025-04-30 12:33:12 +0700

    finish project's architecture

---
+++ app/Models/Job.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Job extends Model
+{
+    use HasFactory;
+    protected $guarded = ['id'];
+    public function employer(): BelongsTo
+    {
+        return $this->belongsTo(Employer::class);
+    }
+    public function tags(): belongsToMany{
+        return $this->belongsToMany(Tag::class);
+    }
+}
--- resources/views/components/job-card-wide.blade.php
+++ resources/views/components/job-card-wide.blade.php
@@ -1,18 +1,20 @@
+@props(['job'])
 <x-panel class="flex gap-x-6">
     <div>
         <x-employer-logo></x-employer-logo>
     </div>
 
     <div class="flex-1 flex flex-col">
-        <div class="self-start text-sm text-gray-600 transition-colors duration-300">Laracasts</div>
-        <h3 class="group-hover:text-blue-800 font-bold text-xl mt-3">Video Producer</h3>
-        <p class="text-sm text-gray-400 mt-auto">Full Time - From $60,000</p>
+        <div class="self-start text-sm text-gray-600 transition-colors duration-300">{{$job->employer->name}}</div>
+        <h3 class="group-hover:text-blue-800 font-bold text-xl mt-3">{{$job->title}}</h3>
+        <p class="text-sm text-gray-400 mt-auto">{{$job->schedule}} - From {{$job->salary}}</p>
     </div>
 
     <div>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
-        <x-tag>Tag</x-tag>
+        @foreach($job->tags as $tag)
+            <x-tag :$tag/>
+        @endforeach
+
     </div>
 
 </x-panel>
